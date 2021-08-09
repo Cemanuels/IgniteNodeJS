2650226a9d41202a99aa4a2e847b497a4de90586
Cemanuels <c.emanuel_ufc@outlook.com>
2021-08-09 02:24:08 -0300

    create categories model repository

--- typescript/src/routes/categories.routes.ts
+++ typescript/src/routes/categories.routes.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
+categoriesRoutes.get("/", (request, response) => {
+  if (!categoriesRepository) {
+    return response.status(400).json([]);
+  }
 
-categoriesRoutes.post("/categories", (request, response) => {
+  return response.status(200).json(categoriesRepository.categories);
+});
+
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
